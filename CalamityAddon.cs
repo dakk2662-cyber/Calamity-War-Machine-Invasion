e3265a7f68b0f2bbed6cb0baa8aed79b91e3d85d
dakk2662-cyber <dakk2662@gmail.com>
2026-06-22 02:35:43 +0300

    Add files via upload

--- CalamityAddon.cs
+++ CalamityAddon.cs
@@ -2,8 +2,10 @@ using CalamityAddon.Content.Events;
 using CalamityAddon.Content.Items.Summons;
 using MonoMod.RuntimeDetour;
 using System;
+using System.IO;
 using System.Reflection;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace CalamityAddon
@@ -13,8 +15,15 @@ namespace CalamityAddon
         private static Hook rightClickHook;
         private static Func<Func<object, int, int, bool>, object, int, int, bool> ourDetour;
 
+        // Идентификаторы сетевых сообщений
+        internal enum MessageType : byte
+        {
+            RequestStartInvasion
+        }
+
         public override void Load()
         {
+            // Пытаемся найти тип тайла приманки из Calamity
             Type targetTileType = Type.GetType("CalamityMod.Tiles.Furniture.WulfrumLure, CalamityMod");
             if (targetTileType != null)
             {
@@ -30,17 +39,53 @@ namespace CalamityAddon
         public override void Unload()
         {
             rightClickHook?.Dispose();
+            rightClickHook = null;
+            ourDetour = null;
         }
+
         private static bool RightClick_Detour(Func<object, int, int, bool> orig, object self, int i, int j)
         {
-            if (Main.LocalPlayer.HasItem(ModContent.ItemType<UnstableCore>()) && !WulfrumRush.isInvasionActive)
+            Player player = Main.LocalPlayer;
+            int batteryType = ModContent.ItemType<UnstableBattery>();
+
+            // Если у игрока есть батарейка и нашествие не активно
+            if (player.HasItem(batteryType) && !WulfrumRush.isInvasionActive)
             {
-                WulfrumRush.StartInvasion();
-                Main.LocalPlayer.ConsumeItem(ModContent.ItemType<UnstableCore>(), true); //Замени трубу на свою призывалку
-                return true;
+                if (Main.netMode == NetmodeID.MultiplayerClient)
+                {
+                    // В мультиплеере отправляем пакет-запрос на сервер
+                    var packet = ModContent.GetInstance<CalamityAddon>().GetPacket();
+                    packet.Write((byte)MessageType.RequestStartInvasion);
+                    packet.Send();
+                }
+                else
+                {
+                    // В одиночной игре запускаем нашествие напрямую
+                    WulfrumRush.StartInvasion();
+                }
+                return true; // Возвращаем true, чтобы не срабатывала стандартная логика приманки
             }
-            else
-                return orig(self, i, j);
+
+            return orig(self, i, j);
+        }
+
+        // Обработка сетевых пакетов
+        public override void HandlePacket(BinaryReader reader, int whoAmI)
+        {
+            MessageType msgType = (MessageType)reader.ReadByte();
+
+            if (msgType == MessageType.RequestStartInvasion)
+            {
+                // whoAmI в данном методе — это индекс игрока, который отправил пакет
+                Player player = Main.player[whoAmI];
+                int batteryType = ModContent.ItemType<UnstableBattery>();
+
+                // Сервер проверяет: жив ли игрок, есть ли у него батарейка и не идет ли уже нашествие
+                if (player.active && !player.dead && player.HasItem(batteryType) && !WulfrumRush.isInvasionActive)
+                {
+                    WulfrumRush.StartInvasion();
+                }
+            }
         }
     }
 }
