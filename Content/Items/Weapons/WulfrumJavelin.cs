076e7890e33b5efe3bdb807fb05bdf5e9c77ffc1
dakk2662-cyber <dakk2662@gmail.com>
2026-06-23 01:40:22 +0300

    Add files via upload

--- Content/Items/Weapons/WulfrumJavelin.cs
+++ Content/Items/Weapons/WulfrumJavelin.cs
@@ -1,11 +1,11 @@
 using CalamityMod;
-using CalamityMod.CalPlayer;
-using CalamityMod.Items;
 using CalamityMod.Items.Weapons.Rogue;
 using CalamityAddon.Content.Projectiles.Rogue;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
 
 namespace CalamityAddon.Content.Items.Weapons
 {
@@ -15,20 +15,31 @@ namespace CalamityAddon.Content.Items.Weapons
         {
             Item.width = 44;
             Item.height = 44;
-            Item.damage = 15;
+            Item.damage = 14;
             Item.DamageType = ModContent.GetInstance<RogueDamageClass>();
             Item.noMelee = true;
             Item.noUseGraphic = true;
-            Item.useAnimation = 30;
-            Item.useTime = 30;
-            Item.useStyle = ItemUseStyleID.Shoot;
-            Item.knockBack = 3f;
-            Item.UseSound = null; // Звук будет при броске
+            Item.useAnimation = 35;
+            Item.useTime = 35;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 4f;
+            Item.UseSound = SoundID.Item1;
             Item.autoReuse = true;
-            Item.shoot = ModContent.ProjectileType<WulfrumJavelinHeld>();
-            Item.shootSpeed = 1f;
+            Item.shoot = ModContent.ProjectileType<WulfrumJavelinProj>();
+            Item.shootSpeed = 11f;
             Item.rare = ItemRarityID.Blue;
             Item.value = Item.buyPrice(0, 0, 10, 0);
+        }
+
+        public override float StealthDamageMultiplier => 1.5f;
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Vector2 shootFrom = player.MountedCenter;
+
+            Projectile.NewProjectile(source, shootFrom, velocity, type, damage, knockback, player.whoAmI);
+
+            return false;
         }
     }
 }
