5d015b5b77d6bfb62bbb0ba41800b8685102a3e2
dakk2662-cyber <dakk2662@gmail.com>
2026-07-06 16:21:55 +0300

    Add files via upload

---
+++ KeybindSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace CalamityAddon.Content
+{
+    public class KeybindSystem : ModSystem
+    {
+        public static ModKeybind WDASKeybind { get; private set; }
+
+        public override void Load()
+        {
+            WDASKeybind = KeybindLoader.RegisterKeybind(Mod, "WDASKeybind", "N");
+        }
+
+        public override void Unload()
+        {
+            WDASKeybind = null;
+        }
+    }
+}
